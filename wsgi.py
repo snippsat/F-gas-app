ee6602f1869383e0115a598e0021faa5455ee9a7
Tom Egil Larsen <teglarse@online.no>
2025-04-28 20:27:41 +0200

    Fix Gunicorn configuration for Render deployment

--- wsgi.py
+++ wsgi.py
@@ -8,5 +8,8 @@ app = create_app()
 # Ensure migrate is registered in this context as well
 migrate = Migrate(app, db)
 
+# This is the variable that Gunicorn will look for
+application = app
+
 if __name__ == '__main__':
     app.run() 
